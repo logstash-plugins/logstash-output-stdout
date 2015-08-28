90094a6c0ad94973b48c4ff3c8c8cf838467b0eb
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-28 19:12:27 +0200

    add basic test for the plugin

--- spec/outputs/stdout_spec.rb
+++ spec/outputs/stdout_spec.rb
@@ -1,4 +1,29 @@
-require 'logstash/outputs/stdout'
+# encoding: utf-8
+require_relative "../spec_helper"
 
 describe LogStash::Outputs::Stdout do
+
+  it "should register without errors" do
+    plugin = LogStash::Plugin.lookup("output", "stdout").new({})
+    expect { plugin.register }.to_not raise_error
+  end
+
+  describe "#send" do
+
+    subject { LogStash::Outputs::Stdout.new({}) }
+
+    let(:properties) { { "message" => "This is a message!"} }
+    let(:event)      { LogStash::Event.new(properties) }
+
+    before(:each) do
+      subject.register
+    end
+
+    it "sends the generated event out" do
+      expect(subject.codec).to receive(:encode).with(event)
+      subject.receive(event)
+    end
+
+  end
+
 end
